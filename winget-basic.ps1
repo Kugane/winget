8353877c0c37ee338d078dbbed440db7825a99bb
Kugane <andreasmontnacher@gmail.com>
2021-12-05 18:11:16 +0100

    rc v.1.1
    i'm stupid :D

--- winget-basic.ps1
+++ winget-basic.ps1
@@ -22,22 +22,11 @@ elseif ($OSVersion -lt "10.0.22000" -and !$hasPackageManager.Version -lt "2021.1
     Add-AppxPackage -Path https://aka.ms/Microsoft.VCLibs.x86.14.00.Desktop.appx
     Add-AppxPackage -Path https://aka.ms/Microsoft.VCLibs.x64.14.00.Desktop.appx
 
-<<<<<<< Updated upstream
-        Add-AppxPackage -Path https://aka.ms/Microsoft.VCLibs.x86.14.00.Desktop.appx
-        Add-AppxPackage -Path https://aka.ms/Microsoft.VCLibs.x64.14.00.Desktop.appx
-
-		$releases_url = "https://api.github.com/repos/microsoft/winget-cli/releases/latest"
+	    $releases_url = "https://api.github.com/repos/microsoft/winget-cli/releases/latest"
 		[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 		$releases = Invoke-RestMethod -uri "$($releases_url)"
 		$latestRelease = $releases.assets | Where { $_.browser_download_url.EndsWith("msixbundle") } | Select -First 1
 		Add-AppxPackage -Path $latestRelease.browser_download_url
-=======
-	$releases_url = "https://api.github.com/repos/microsoft/winget-cli/releases/latest"
-	[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-	$releases = Invoke-RestMethod -uri "$($releases_url)"
-	$latestRelease = $releases.assets | Where { $_.browser_download_url.EndsWith("msixbundle") } | Select -First 1
-	Add-AppxPackage -Path $latestRelease.browser_download_url
->>>>>>> Stashed changes
 
     Write-Host -ForegroundColor Green "WinGet successfully installed."
 }
